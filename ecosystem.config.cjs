824f0f90c031001f1e5e925affea7c589f31a5e8
soulwax <soulwax-95@protonmail.com>
2026-01-01 07:05:33 +0000

    feat: add PM2 configuration and update dependencies for Neon Postgres

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+// ecosystem.config.js for a sveltekit project using pm2
+module.exports = {
+  apps: [
+    {
+      name: 'zodiac-svelte',
+        script: 'node_modules/.bin/svelte-kit',
+        args: 'start',
+        env: {
+            NODE_ENV: 'production',
+            PORT: process.env.PORT || 4332
+        }
+    }
+  ]
+};
